7ac61d78eaef6a52cbf8ddc1f1d3ace3d6fba59a
AIZAWA Hina <hina@fetus.jp>
2022-02-01 11:56:37 +0900

    2022

--- src/internal/PostalCodeBootstrap.php
+++ src/internal/PostalCodeBootstrap.php
@@ -2,7 +2,7 @@
 
 /**
  * @author AIZAWA Hina <hina@fetus.jp>
- * @copyright 2015 by AIZAWA Hina <hina@fetus.jp>
+ * @copyright 2015-2022 by AIZAWA Hina <hina@fetus.jp>
  * @license https://github.com/fetus-hina/yii2-jp-postalcode-validator/blob/master/LICENSE MIT
  */
 
